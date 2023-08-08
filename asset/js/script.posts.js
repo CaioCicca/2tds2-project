460a6700c1896d8b527de903d361c234242f657c
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2023-08-08 15:10:24 -0300

    feat:javascript and html finished

--- asset/js/script.posts.js
+++ asset/js/script.posts.js
@@ -8,11 +8,12 @@ function savePost() {
     const date = document.getElementById("date").value;
 
     if(title && category && resume && publisher && date){
-        storePosts(title, category, resume, publisher, date)
+        storePosts(title, category, resume, publisher, date);
+        cleanFields();
+        showPosts();
     } else{
         alert("Preencha todos os campos! ")
     }
-    cleanFields();
 }
 
 function cleanFields() {
@@ -33,4 +34,25 @@ function storePosts(title, category, resume, publisher, date){
     };
     posts.push(post)
     console.log(posts)
+}
+
+function showPosts(){
+    console.log("entrou aqui")
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="itemPost">
+            <h2>${post.title}</h2>
+            <p><strong>Category: </strong>${post.category}</p>
+            <p><strong>Resumo: </strong>${post.resume}</p>
+            <p><strong>Autor: </strong>${post.publisher}</p>
+            <p><strong>Data de publicação: </strong>${post.date}</p>
+
+            <button onclick="editPost(${index})">Editar</button>
+            <button onclick="deletePost(${index})">Excluir</button>
+        </div>`;
+    });
+
+    document.getElementById("list").innerHTML = showContent;
 }
