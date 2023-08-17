5388a4f9a75b9ce8d36947c80613a825e0598366
Caio Ciccarelli <caio.ciccarelli@aluno.senai.br>
2023-08-17 11:14:00 -0300

    feat: create class ProductService

--- asset/js/script.category.js
+++ asset/js/script.category.js
@@ -18,7 +18,7 @@ class Product {
 
 class CategoryService {
     constructor(){
-        this.categorys = [];
+        this.categories = [];
         this.nextCategoryId = 0;
     }
 
@@ -27,5 +27,27 @@ class CategoryService {
         const category = new Category(id, name);
         this.categories.push(category);
     }
+}
 
+class ProductService{
+    constructor(){
+        this.products = [];
+        this.nextProductId = 0;
+    }
+
+    addProduct(name, price, category){
+        const id = this.nextProductId++;
+        const product = new Product(id, name, price, category);
+        category.products.push(product);
+        this.products.push(product);
+    }
+}
+
+const categoryList = new CategoryService()
+
+function createCategory(){
+    const categoryName = "Doce";
+
+    categoryList.addCategory(categoryName)
+    console.log(categoryList.categories)
 }
